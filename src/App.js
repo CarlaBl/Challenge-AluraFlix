62311e79c73fc109df376ab9dfe8c732264bcb1d
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2024-07-16 18:26:43 -0600

    creamos la pagina de nueva categoria

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './paginas/Home'
 import Header from './componentes/Header/Header'
 import Footer from './componentes/Footer/Footer'
-import NuevoVideo from './paginas/NuevoVideo';
+import NuevoVideo from './paginas/NuevoVideo'
+import NuevaCategoria from './paginas/NuevaCategoria'
 import Pagina404 from './paginas/Pagina404'
 import GlobalStyle from './GloblalStyle'
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root'); // Define el elemento de la aplicación
 function App () {
     return (
         <div className="App">
@@ -16,6 +19,7 @@ function App () {
                 <Routes>
                 <Route path='/' element={ <Home /> } />
                 <Route path='/nuevovideo' element={ <NuevoVideo /> }/>
+                <Route path='/nuevacategoria' element={ <NuevaCategoria /> }/>
                 <Route path='*' element={ <Pagina404/> } />
                 
                 </Routes>
